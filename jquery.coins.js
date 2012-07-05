729fdb2f04faecdea031ee8a9e13635b333a6070
Matt Murphy <mmmurf@gmail.com>
2012-07-05 02:05:28 -0700

    Update

--- jquery.coins.js
+++ jquery.coins.js
@@ -28,13 +28,13 @@
 	    var generate_coin = function(){
 		var top = Math.floor(Math.random()*settings.height)
 		var left = Math.floor(Math.random()*settings.width)
-		var coin = $('<img class="jq_coins" src="mario.gif" style="height: 50px">')
+		var coin = $('<img class="jq_coins" src="/images/mario.gif" style="height: 50px">')
 
 		var c = coin.clone().css({position: 'absolute', top: top +'px', left: left +'px'})
 
 		c.mouseover(function(){
 		    score += settings.points;
-		    var aud = $('<audio autoplay="autoplay"><source src="mario.ogg" type="audio/ogg" /></audio>').clone().remove()
+		    var aud = $('<audio autoplay="autoplay"><source src="/audio/mario.ogg" type="audio/ogg" /></audio>').clone().remove()
 
 		    coins_left -= 1;
 		    $(this).hide().remove()
